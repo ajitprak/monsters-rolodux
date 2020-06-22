36f8d969080f04b9a4f1f211fec2e2d159af73e6
Ajith <aprakash@broadsoft.com>
2020-06-22 18:21:09 +0530

    Updates

---
+++ precache-manifest.4129f02594427aa393e0354b96496807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace84d7cdaf0a17d39e59c326075a050",
+    "url": "/monsters-rolodux/index.html"
+  },
+  {
+    "revision": "6c0f7de239cb3c8425f2",
+    "url": "/monsters-rolodux/static/css/main.4c6b9500.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodux/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodux/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c0f7de239cb3c8425f2",
+    "url": "/monsters-rolodux/static/js/main.5bb8c0fb.chunk.js"
+  },
+  {
+    "revision": "3060534c6a9edb583f85",
+    "url": "/monsters-rolodux/static/js/runtime-main.dbfc253d.js"
+  }
+]);
